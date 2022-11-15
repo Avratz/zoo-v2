335a1b422c85fdd0d160cc4e5b8cc62015e6cca4
Avratz <signos97@gmail.com>
2022-11-15 13:46:15 -0300

    html structure

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Zoo</h1>
+  <div class="zoo">
+    <h2>Make your favorite animal speak!</h2>
+
+    <label for="phrase">
+      Phrase:
+      <input type="text" id="phrase" name="phrase" placeholder="Set a phrase" />
+    </label>
+
+    <ul class="zoo-list">
+      <li data-id="wookie" data-type="animal">
+        <img src="/animals/chewie.png" alt="lion" width="70px" height="70px" />
+        <h3>Wookie</h3>
+      </li>
+      <li data-id="lion" data-type="animal">
+        <img src="/animals/lion.png" alt="lion" width="70px" height="70px" />
+        <h3>Lion</h3>
+      </li>
+      <li data-id="snake" data-type="animal">
+        <img src="/animals/snake.png" alt="lion" width="70px" height="70px" />
+        <h3>Snake</h3>
+      </li>
+      <li data-id="tiger" data-type="animal">
+        <img src="/animals/tiger.png" alt="lion" width="70px" height="70px" />
+        <h3>Tiger</h3>
+      </li>
+      <li data-id="elephant" data-type="animal">
+        <img src="/animals/elephant.png" alt="lion" width="70px" height="70px" />
+        <h3>Elephant</h3>
+      </li>
+      <li data-id="wolf" data-type="animal">
+        <img src="/animals/wolf.png" alt="lion" width="70px" height="70px" />
+        <h3>Wolf</h3>
+      </li>
+    </ul>
+    <div class="phrase hidden" id="phrase-output">
+      <h3 id="animal-selected"></h3>
+      <h4 id="phrase-spoken"></h4>
+    </div>
+  </div>
+`;
